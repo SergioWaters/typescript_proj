2c14724aecbdbc2926024ed9528f91ad3e179aff
Serj_NOTE <mokretsovsa@gmail.com>
2022-09-27 20:51:29 +0300

    logo has been changed

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,10 @@ import { renderUserBlock } from './user.js'
 
 
 window.addEventListener('DOMContentLoaded', () => {
-  renderUserBlock(1, '', '');
+  renderUserBlock(
+    7,
+    'https://sun9-50.userapi.com/impf/73QEYamxaMe5prXb3LoC2-MDGIdf8A6ecE0L2Q/q_Si_lrspqQ.jpg?size=130x130&quality=96&sign=208915f9d0cd44fd33c237ae0b40f7d1&c_uniq_tag=3jDVX7lOnvmTKgmauOnu6HM7r51g96YynxP_Stf_MxM&type=album',
+    'Sergio Waters');
   renderSearchFormBlock(null, null);
   renderSearchStubBlock();
   // renderToast(
